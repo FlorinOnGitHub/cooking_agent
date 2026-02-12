30ae29173c2fc9b8b9ab42c69182f6a2f899da66
FlorinPriboi <florinpriboi@flawlessworkflow.com>
2026-02-12 19:11:20 +0100

    improved output printing

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from langchain_core.messages import HumanMessage
 from agent import CookingAgent
 from rich.markdown import Markdown
 from rich.console import Console
-
 load_dotenv()
 
 async def show_spinner():
@@ -39,23 +38,24 @@ async def main():
     console.print(Markdown("\n# What are you craving?"))
 
     while True:
-        user_input = input("[USER]")
+        user_input = input("[USER]\n")
         if user_input.lower() in ["quit", "exit"]:
             break
 
         inputs = {"messages": [HumanMessage(content=user_input)]}
-
-        # Run the agent
+        print("\n")
         console.print(Markdown("[POCKET GORDON RAMSAY]"))
         spinner_task = asyncio.create_task(show_spinner())
         async for event in bot.graph.astream(inputs, config=config, stream_mode="values"):
             message = event["messages"][-1]
             if message.type == "ai":
                 spinner_task.cancel()
+                sys.stdout.write("\r" + " " * 50 + "\r") 
+                sys.stdout.flush()
                 console.print(Markdown(message.content))
-
+                sys.stdout.flush()
         console.print("\n")
-        console.print(Markdown("Anything else I can help you with"))
+        console.print(Markdown("Anything else I can help you with?\n"))
 
 if __name__ == "__main__":
     asyncio.run(main())
